62f771b9e6dc952cba7210c58921873bbc3b5809
Neehar Patil <pluckynumbat@gmail.com>
2024-12-03 13:40:19 +0530

    created the staxlib test file with basic table driven tests for the IsNil() method

---
+++ staxlib/staxlib_test.go
@@ -0,0 +1,27 @@
+package staxlib
+
+import "testing"
+
+func TestIsNil(t *testing.T) {
+	var s1, s2 *Stack
+	s2 = &Stack{}
+	var tests = []struct {
+		name string
+		s    *Stack
+		want bool
+	}{
+		{"nil true", s1, true},
+		{"nil false", s2, false},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			want := test.want
+			got := test.s.IsNil()
+
+			if want != got {
+				t.Errorf("IsNil returned incorrected results, want: %v, got: %v", want, got)
+			}
+		})
+	}
+}
