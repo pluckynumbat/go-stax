1b9cb8d89a5b6c27010ae71eab68faaf39da4a3a
Neehar Patil <pluckynumbat@gmail.com>
2025-01-10 17:40:00 +0530

    added tests for the basic state methods on the semi generic stack

---
+++ sgstaxlib/sgstaxlib_test.go
@@ -0,0 +1,107 @@
+package sgstaxlib
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/pluckynumbat/linked-list-stuff-go/sdlistlib"
+)
+
+type prInt int
+
+func (p *prInt) String() string {
+	return fmt.Sprintf("%v", *p)
+}
+
+func TestIsNil(t *testing.T) {
+
+	var s1 *SemiGenericStack[*prInt]
+	s2 := &SemiGenericStack[*prInt]{}
+
+	var tests = []struct {
+		name  string
+		stack *SemiGenericStack[*prInt]
+		want  bool
+	}{
+		{"nil true", s1, true},
+		{"nil false", s2, false},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			got := test.stack.IsNil()
+			want := test.want
+
+			if got != want {
+				t.Errorf("IsNil() gave incorrect results, want: %v, got : %v", want, got)
+			}
+		})
+	}
+}
+
+func TestIsListNil(t *testing.T) {
+
+	var s1 *SemiGenericStack[*prInt]
+	s2 := &SemiGenericStack[*prInt]{}
+
+	l := &sdlistlib.SemiGenericList[*prInt]{}
+	s3 := &SemiGenericStack[*prInt]{l}
+
+	var tests = []struct {
+		name  string
+		stack *SemiGenericStack[*prInt]
+		want  bool
+	}{
+		{"nil stack", s1, true},
+		{"nil list", s2, true},
+		{"nil false", s3, false},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			got := test.stack.isListNil()
+			want := test.want
+
+			if got != want {
+				t.Errorf("isListNil() gave incorrect results, want: %v, got : %v", want, got)
+			}
+		})
+	}
+}
+
+func TestIsEmpty(t *testing.T) {
+
+	var s1 *SemiGenericStack[*prInt]
+	s2 := &SemiGenericStack[*prInt]{}
+
+	l := &sdlistlib.SemiGenericList[*prInt]{}
+	s3 := &SemiGenericStack[*prInt]{l}
+
+	var pr prInt = 1
+	var ptr = &pr
+	l2 := &sdlistlib.SemiGenericList[*prInt]{}
+	l2.AddAtBeginning(ptr)
+	s4 := &SemiGenericStack[*prInt]{l2}
+
+	var tests = []struct {
+		name  string
+		stack *SemiGenericStack[*prInt]
+		want  bool
+	}{
+		{"nil stack", s1, true},
+		{"non nil stack, nil list", s2, true},
+		{"non nil stack, empty list", s3, true},
+		{"non nil stack, non empty list", s4, false},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			got := test.stack.IsEmpty()
+			want := test.want
+
+			if got != want {
+				t.Errorf("IsEmpty() gave incorrect results, want: %v, got : %v", want, got)
+			}
+		})
+	}
+}
